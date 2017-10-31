ea95edcd3f0a229130eb9ae5873c441eff76fb54
broitsme <rahulbhatia1313@gmail.com>
2017-10-31 18:24:26 +0530

    new solution in mixed problems

--- src/mixedProblems.java
+++ src/mixedProblems.java
@@ -2,7 +2,94 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 public class mixedProblems {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfGates = scanner.nextInt();
+        int[][] gates = new int[numberOfGates][2];
+        for (int i = 0; i < numberOfGates; i++) {
+            gates[i][0] = scanner.nextInt();
+            gates[i][1] = scanner.nextInt();
+        }
+        System.out.println(tollGate.solveTollGateProblem(gates));
+        //System.out.println(time);
+    }
+    static long time = 1;
+    static class tollGate {
+        private static int min = Integer.MAX_VALUE;
+        static int solveTollGateProblem(final int[][] gates) {
+            solver(gates, 0, new int[3], 0);
+            return min;
+        }
+
+        private static void solver(final int gates[][], int indexOfGates, int[] menForFight, int pricePaid) {
+            time++;
+            if (indexOfGates == gates.length) {
+                if (pricePaid < min) {
+                    min = pricePaid;
+                }
+                return;
+            }
+
+            int totalMenForFight = getTotalMen(menForFight);
+            if (totalMenForFight >= gates[indexOfGates][0]) {
+                int[] menForFightUpdated = returnUpdateAfterFight(menForFight, gates[indexOfGates][0]);
+                solver(gates, indexOfGates + 1, menForFightUpdated, pricePaid);
+            }
+            solver(gates, indexOfGates + 1, menForFight, pricePaid + gates[indexOfGates][1]);
+            menForFight[2] += gates[indexOfGates][0];
+            solver(gates, indexOfGates + 1, menForFight, pricePaid + (2 * gates[indexOfGates][1]));
+            menForFight[2] -= gates[indexOfGates][0];
+        }
+        private static int[] returnUpdateAfterFight(final int[] menForFight, int men) {
+            int i = 0;
+            int[] update = new int[3];
+            while (i < menForFight.length && men != 0) {
+                if (menForFight[i] <= men) {
+                    update[i] = 0;
+                    men -= menForFight[i];
+                } else {
+                    update[i] = menForFight[i] - men;
+                    men = 0;
+                }
+                i++;
+            }
+            while (i < menForFight.length) {
+                update[i] = menForFight[i];
+                i++;
+            }
+            update[0] = update[1];
+            update[1] = update[2];
+            update[2] = 0;
+            return update;
+        }
+
+        private static int getTotalMen(int[] menForFight) {
+            int sum = 0;
+            for (int val : menForFight) {
+                sum += val;
+            }
+            return sum;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /* public static void main(String[] args) {
             Scanner s = new Scanner(System.in);
             int test = s.nextInt();
             while (test-- > 0) {
@@ -49,7 +136,7 @@ public class mixedProblems {
                     System.out.println("NO");
                 }
             }
-        }
+        }*/
     static void wordBreak(String str, int index, HashSet<String> strings, String ansTillNow) {
         if (index == str.length()) {
             System.out.println(ansTillNow);
